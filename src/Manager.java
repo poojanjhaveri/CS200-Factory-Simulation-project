740c5c650e76125b3d0ced6f023f5040f1b428ee
Yiwei Zheng <yiweizhe@usc.edu>
2012-10-25 20:05:11 -0700

    adding Manager

---
+++ src/Manager.java
@@ -0,0 +1,22 @@
+
+/**
+The Manager plays a pivotal role in server interaction by standardizing the methods used to send up data to the FactoryProductionManager simulation GUI.
+@brief used to standardize methods for all three managers
+*/
+
+interface Manager {
+
+Manager()
+{
+
+}
+
+/**
+@brief passes all relevant state information to the main Factory Production Manager for painting
+*/
+public void giveState()
+{
+
+}
+
+}
