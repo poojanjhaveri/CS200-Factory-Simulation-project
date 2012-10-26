9d38b61205715f2cc562c5c14eedb90eb85057ed
Yiwei Zheng <yiweizhe@usc.edu>
2012-10-25 20:56:49 -0700

    adding drawable

---
+++ src/Drawable.java
@@ -0,0 +1,10 @@
+/**
+@brief anything drawable onto the FactoryProductionManager Frame.
+@author YiWei Roy Zheng
+*/
+public class Drawable{
+
+Integer posX;///<current X-coordinate of the drawable
+Integer poxY;///<current Y-coordinate of the drawable
+
+}
--- src/Robot.java
+++ src/Robot.java
@@ -1,10 +1,12 @@
-
+/**
+@brief a Robotic device that manipulates its surroundings
+@author YiWei Roy Zheng
+*/
 public class Robot extends Drawable{
 
 static final Integer velocityX;///<pixels/s speed X direction of Robot
 static final Integer velocityY;///<pixels/s speed Y direction of Robot
 
-Integer posX;///<current X-coordinate of the Robot
-Integer poxY;///<current Y-coordinate of the Robot
+
 
 }
