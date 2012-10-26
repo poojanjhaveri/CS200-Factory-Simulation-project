b8b4ebaafd7899398660230b5db012fd9a0f9b09
Yiwei Zheng <yiweizhe@usc.edu>
2012-10-25 20:54:57 -0700

    adding Robot

---
+++ src/Robot.java
@@ -0,0 +1,10 @@
+
+public class Robot extends Drawable{
+
+static final Integer velocityX;///<pixels/s speed X direction of Robot
+static final Integer velocityY;///<pixels/s speed Y direction of Robot
+
+Integer posX;///<current X-coordinate of the Robot
+Integer poxY;///<current Y-coordinate of the Robot
+
+}
