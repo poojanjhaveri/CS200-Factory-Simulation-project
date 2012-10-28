87cbbe0c76ffc72819e4e5606c2db80bf4e412e9
Alex Young <alexyoung1992@gmail.com>
2012-10-28 04:05:53 -0700

    modified Kit.java and Part.java

--- src/factory/Kit.java
+++ src/factory/Kit.java
@@ -1,12 +1,26 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package factory;
-
 /**
+ * Kit Object
+ * @author James Dalton
  *
- * @author Alex
  */
 public class Kit {
+
+public Part parts[];
+public enum Status {empty, full, inspected};
+public Status status;
+public int kitNeedsParts;
+public static enum KittingStandNumber {none, one, two, three};
+public KittingStandNumber kittingStandNumber;
+
+public Kit(Part p[]){
+this.kittingStandNumber = KittingStandNumber.none;
+this.status = Status.empty;
+this.kitNeedsParts = p.length;
+for (int i=0; i<p.length; i++){
+	parts[i] = p[i];
+}
+}
+
+
+
 }
--- src/factory/Part.java
+++ src/factory/Part.java
@@ -1,10 +1,15 @@
-package factory;
 
 public class Part {
+public enum Type {p1, p2, p3, p4, p5, p6, p7, p8};
+public Type type;
+public boolean inKit;
+public int size;
+//public boolean good; 
 
-    public enum PartType {
+public Part(Type t, boolean inkit, int size){
+	this.type = t;
+	this.inKit = false;
+	this.size = size;
+}
 
-        parta, partb, partc
-    };
-    public PartType type;
 }
