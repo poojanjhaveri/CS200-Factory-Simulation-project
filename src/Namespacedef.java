fb4e8079ce9cabda6fc31a965727c8189b0f23bf
Yiwei Zheng <yiweizhe@usc.edu>
2012-10-28 12:40:37 -0700

    adding namespace definitions to doc

---
+++ src/Namespacedef.java
@@ -0,0 +1,28 @@
+/**
+@namespace factory.agentGUI
+@brief non-UI components such as robots, parts, etc
+*/
+/**
+@namespace factory.controlGUI
+@brief UI components such as control panels
+*/
+/**
+@namespace interfaces
+@brief various interfaces
+*/
+/**
+@namespace agent
+@brief agents and their supporting classes
+*/
+/**
+@namespace factory.feederManagerment
+@brief agents for the feeder antry and lane
+*/
+/**
+@namespace factory.kitManagerment
+@brief agents for the conveyer, camera, and kitrobot
+*/
+/**
+@namespace partsManagerment
+@brief agents for the nest, parts, kits
+*/
