ec6c7ece80e773683df5eaf392d980aa19072352
Alex Young <alexyoung1992@gmail.com>
2012-10-28 04:21:23 -0700

    modified kit and part

--- src/factory/Kit.java
+++ src/factory/Kit.java
@@ -1,26 +1,33 @@
+
+package factory;
+
 /**
  * Kit Object
+ *
  * @author James Dalton
  *
  */
 public class Kit {
 
-public Part parts[];
-public enum Status {empty, full, inspected};
-public Status status;
-public int kitNeedsParts;
-public static enum KittingStandNumber {none, one, two, three};
-public KittingStandNumber kittingStandNumber;
+    public Part parts[];
 
-public Kit(Part p[]){
-this.kittingStandNumber = KittingStandNumber.none;
-this.status = Status.empty;
-this.kitNeedsParts = p.length;
-for (int i=0; i<p.length; i++){
-	parts[i] = p[i];
-}
-}
+    public enum Status {
 
+        empty, full, inspected
+    };
+    public Status status;
+    public int kitNeedsParts;
 
+    public static enum KittingStandNumber {
 
+        none, one, two, three
+    };
+    public KittingStandNumber kittingStandNumber;
+
+    public Kit(Part p[]) {
+        this.kittingStandNumber = KittingStandNumber.none;
+        this.status = Status.empty;
+        this.kitNeedsParts = p.length;
+        System.arraycopy(p, 0, parts, 0, p.length);
+    }
 }
