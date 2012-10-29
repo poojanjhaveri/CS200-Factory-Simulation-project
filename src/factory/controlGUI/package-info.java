245f6716a2ae6685b52e02c0abee0078af05a30f
Yiwei Zheng <yiweizhe@usc.edu>
2012-10-28 21:32:43 -0700

    fixing someone's weird source formatting

--- src/factory/controlGUI/package-info.java
+++ src/factory/controlGUI/package-info.java
@@ -1,6 +1,6 @@
 /**
- * <img src="../img/factory.controlgui.png" /> <img
- * src="../img/factory.controlgui2.png" /> @brief UI components such as control
- * panels
+<img src="../img/factory.controlgui.png" />
+ <img src="../img/factory.controlgui2.png" />
+ @brief UI components such as control panels
  */
 package factory.controlGUI;
