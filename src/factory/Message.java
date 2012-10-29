f47035b8400a727c6ebaeeb2c26e0b163245f013
David Zhang <dzhang21@gmail.com>
2012-10-28 19:10:47 -0700

    Created Server and Printer class; preliminary Message class

---
+++ src/factory/Message.java
@@ -0,0 +1,13 @@
+package factory;
+
+/**
+ * @brief This class stores constant strings to establish clear messages between the server and client.
+ * Protocol: Some messages will be passed with a ":"; anything after this is parameter data (e.g., a number)
+ * @author David Zhang
+ */
+public class Message {
+	public static final String CLIENT_EXITED = "CLIENT_EXITED";
+	
+	public static final String TEST_SERVER = "TEST_SERVER";
+	public static final String TEST_CLIENT = "TEST_CLIENT";
+}
