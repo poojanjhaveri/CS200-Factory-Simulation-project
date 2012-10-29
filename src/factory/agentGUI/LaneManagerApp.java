c9fcbfb2cdf75008e14e7bfb5d0e9cfe21cf465d
wontonst <roycraft3@gmail.com>
2012-10-28 18:50:21 -0700

    docs

--- src/factory/agentGUI/LaneManagerApp.java
+++ src/factory/agentGUI/LaneManagerApp.java
@@ -1,6 +1,12 @@
 package factory.agentGUI;
 import javax.swing.JFrame;
 /**
+- Lane section is shown on left panel and hardware status is shown on right panel. On right panel, the lane manager is able to launch non-normative scenario. Here is lists of hardware status available on right panel
+
+        Feeder : on/off switch, part low sensor, feed parts switch, part fed counter, lower/raise rear gate switch, purge bin switch, diverter left/right switch
+        Long lane : on/off switch, vibration amplitude setting
+        Nest : up/down switch
+        Non-normative scenario
 <img src="../img/image05.png" />
 	@brief shows the lane manager section with graphical Panel and GUI panel
 	@author Dongyoung Jung
