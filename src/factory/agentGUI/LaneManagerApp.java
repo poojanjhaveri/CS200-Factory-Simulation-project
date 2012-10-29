d0174647a38862bc5d8d65aa15b075fe5d1577fb
YiWei Roy Zheng <yiweizhe@DOTSLVL3ZPC01.ics.usc.edu>
2012-10-28 18:48:53 -0700

    img

--- src/factory/agentGUI/LaneManagerApp.java
+++ src/factory/agentGUI/LaneManagerApp.java
@@ -1,6 +1,7 @@
 package factory.agentGUI;
 import javax.swing.JFrame;
 /**
+<img src="../img/image05.png" />
 	@brief shows the lane manager section with graphical Panel and GUI panel
 	@author Dongyoung Jung
 */
