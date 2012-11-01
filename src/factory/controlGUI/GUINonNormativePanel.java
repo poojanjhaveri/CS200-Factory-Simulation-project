9253ecb520868113262a18e76ea7f5db5d4fb32b
David Zhang <dzhang21@gmail.com>
2012-10-31 18:56:53 -0700

    minor fixes

--- src/factory/controlGUI/GUINonNormativePanel.java
+++ src/factory/controlGUI/GUINonNormativePanel.java
@@ -12,7 +12,6 @@ import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 
 /**
-<<<<<<< HEAD
  * @brief Non-normative scenario
  * @author Dongyoung Jung
  */
@@ -46,40 +45,4 @@ public class GUINonNormativePanel extends JPanel implements ActionListener {
             }
         }
     }
-=======
-	@brief Non-normative scenario
-	@author Dongyoung Jung
-*/
-public class GuiNonNormativePanel extends JPanel implements ActionListener{
-
-	JButton button;
-	TitledBorder buttonTitle;
-	ArrayList<JButton> buttons = new ArrayList<JButton>();
-	
-	public GuiNonNormativePanel(){
-		// JPanel setting
-		setPreferredSize(new Dimension(300,640));
-		setLayout(new FlowLayout(FlowLayout.CENTER, 10, 31));
-		
-		// Button generation
-		for(int i=0 ; i<7 ; i++){
-			button = new JButton("example");
-			buttonTitle = new TitledBorder("Non-normative scenario " + (i+1));
-			button.addActionListener(this);
-			button.setBorder(buttonTitle);
-			button.setPreferredSize(new Dimension(200,50));
-			buttons.add(button);
-			add(button);
-		}
-	}
-	
-	public void actionPerformed(ActionEvent ae){
-		for(int i=0 ; i<buttons.size() ; i++){
-			if( ae.getSource() == buttons.get(i) ){
-				// Send Signal to server
-				
-			}
-		}
-	}
->>>>>>> e55bea809a4f01ab018a7b8b99f6378440c1052c
 }
