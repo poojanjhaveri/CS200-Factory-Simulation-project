d346e7965d25a5c3f416b01d19c6069993208d25
James Dalton <jpdalton@usc.edu>
2012-11-04 09:05:07 -0800

    New code fo rNest and partsagent

--- src/factory/factory201/partsManagement/NestAgent.java
+++ src/factory/factory201/partsManagement/NestAgent.java
@@ -1,38 +1,43 @@
-package factory.factory201.partsManagement;
+ public class NestAgent {
 
-import factory.general.Nest;
-import factory.general.Part;
-import factory.factory201.feederManagement.LaneAgent;
-import factory.factory201.kitManagement.CameraAgent;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-/**
- * Nest object has a threshold
- *
- * @brief Nest object has a threshold
- * @author James Dalton
- *
- */
-public class NestAgent {
-  
-
-    private List<Part> parts =
+	private List<Nest> myNests = Collections.synchronizedList(new ArrayList<Nest>());
+	private Map <Nest, LaneAgent> lanes = Collections.synchronizedList(new HashMap<Nest, LaneAgent>);
+    private List<Part> needParts =
             Collections.synchronizedList(new ArrayList<Part>());
     PartsAgent partsagent;
-    LaneAgent lane;
+    
     CameraAgent camera;
-    private int threshold;
-    private Part part;
-    private int howMany = 0;
+    
     private int nestNumber;
     private List<Nest> nests = Collections.synchronizedList(new ArrayList<Nest>());
-
-    //public enum NestNumber { one, two, three, four, five, six, seven, eight};
-    NestAgent(int nestNum, LaneAgent lane) {
+    enum Status {none, needPart, gettingPart, full, gettingInspected, readyForKit, purge};
+    
+    NestAgent()) {
         this.nestNumber = nestNum;
         this.lane = lane;
+    }
+    class Nest{
+    	private int threshold;
+        private Part part;
+        private int howMany = 0;
+        Lane lane;
+        Status status;
+        
+        Nest (Part p){
+        	this.part = p;
+        	this.threshold = 10/p.size;
+        	this.status = Status.needPart;
+        }
+        
+        public void setPart(Part p){
+        	this.part = p;
+        	this.threshold = 10/p.size;
+        }
+        
+        public void setLane(Lane lane){
+        	this.lane = lane;
+        }
+        
     }
 //messages
 
@@ -42,38 +47,103 @@ public class NestAgent {
      * @param p This is a part
      */
     public void msgNeedPart(Part p) {
-        if (part == p) {
-            partsagent.msgHereAreParts(part, howMany);
-        } else {
-            lane.msgNeedPart(p);
-        }
+    	if (!hasPart())
+    	myNests.add(new Nest(p));
+        stateChanged();
     }
 
     public void msgHereAreParts(Part p, int quantity) {
         part = p;
-        threshold = 10 / p.size;
         howMany += quantity;
-        if (howMany > threshold) {
-            this.lane.msgRejectParts(howMany - threshold);
-            howMany = threshold;
-            camera.msgNestIsFull(new Nest(), nestNumber);
-        }
-
+        myNests.getNest(p).status = Status.full;
+        stateChanged();
     }
 
-    public void msgNestInspected(boolean result) {
-        partsagent.msgHereAreParts(part, howMany);
+    public void msgNestInspected(Nest n, boolean result) {
+    	if (result){
+    		n.status = Status.readyForKit;
+    	}
+    	else
+    		n.status = Status.purge;
+    	stateChanged();
     }
     //scheduler
 
     protected boolean pickAndExecuteAnAction() {
-
+    	
+    	if (!myNests.isEmpty()){
+    		
+    		for (Nest n: myNests){
+    			if (n.status == Status.needPart)
+    				requestPart(n);
+    				return true;
+    		}
+    		
+    		for (Nest n: myNests){
+    			if (n.status == Status.full)
+    				requestInspection(n);
+    			return true;
+    		}
+    	}
+    	
+    		for (Nest n: myNests){
+    			if (n.status == Status.readyForKit){
+    				giveToKit(n);
+    				return true;
+    			}
+    		}
+    		
+    		for (Nest n: myNests){
+    			if (n.status == Status.purge){
+    				purge(n);
+    				return true;
+    			}
+    		}
         return false;
     }
-
-    //actions
-    public void setPartsAgent(PartsAgent parts) {
+    
+    public Nest getNest(Part p){
+    	for (Nest n: MyNests){
+    		if(n.part.equals(p))
+    			return n;
+    	}
+    }
+    
+    
+    private void requestPart(Nest n){
+    	n.lane.msgNeedPart(n.part);
+    	n.status = Status.gettingPart;
+    	stateChanged();
+    }
+    
+    private void requestInspection(Nest n){
+    	camera.msgNestIsFull(this);
+    	n.status = Status.gettingInspected;
+    	stateChanged();
+    }
+    
+    private void giveToKit(Nest n){
+    	partsagent.msgHereIsPart(Part p);
+    	n.howMany--;
+    	if (howMany==0)
+    		n.status = Status.needPart;
+    	stateChanged();	
+    }
+    
+    private void purge(Nest n){
+    n.howMany = 0;
+    DoPurge();
+    n.status = Status.needPart;
+    stateChanged();
+    }
+    
+    public boolean hasPart(Part p){
+    	for (Nest n: myNest){
+    	if (n.part.equals(p))
+    		return true;	}
+    	return false;
+    }
+    
+    public void setPartsAgent(Parts parts) {
         this.partsagent = parts;
     }
-//    getPart
-}
--- src/factory/factory201/partsManagement/PartsAgent.java
+++ src/factory/factory201/partsManagement/PartsAgent.java
@@ -1,10 +1,10 @@
-package factory.factory201.partsManagement;
+package factory.partsManagement;
 
 import agent.Agent;
 import factory.ConfigFile;
-import factory.general.Kit;
-import factory.general.Part;
-import factory.factory201.kitManagement.KitRobotAgent;
+import factory.Kit;
+import factory.Part;
+import factory.kitManagement.KitRobotAgent;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -22,45 +22,44 @@ import java.util.Map;
  *
  */
 public class PartsAgent extends Agent {
+
     KitRobotAgent kitagent;
     Kit kit;
     NestAgent nest;
-//nest array needed
-    private List<ConfigFile> configInfo =
-            Collections.synchronizedList(new ArrayList<ConfigFile>());
-    private Map<Part, Integer> inventory = new HashMap<Part, Integer>();
-    Part grips[];
+    private List<Part> inventory =
+            Collections.synchronizedList(new ArrayList<Part>());
+    private List<Part> grips =
+            Collections.synchronizedList(new ArrayList<Part>());
+    private List<Part> kitNeedsParts =
+            Collections.synchronizedList(new ArrayList<Part>());
+    private List<Kit> newKit =
+            Collections.synchronizedList(new ArrayList<Kit>());
 
-    public class Grip {
-
-        public boolean occupied;
+//Messages 
+    public void msgHereIsKit(Kit k){
+    	newKit.add(kit);
+    	stateChanged();
+    	
     }
-
-//Messages
-    public void msgHereIsConfig(ConfigFile file) {
-        configInfo.add(file);
+      
+    public void msgHereIsPart(Part p) {
+        inventory.add(p);
         stateChanged();
     }
 
-    public void msgHereAreParts(Part p, int quantity) {
-        inventory.put(p, quantity);
-        stateChanged();
-    }
-
-    public void msgEmptyKitReady(Kit kit) {
-        int num = 1;
+    public void msgEmptyKitReady(int num) {
         switch (num) {
             case 1:
-                kit.standNum = Kit.StandNum.one;
+                kit.kittingStandNumber = Kit.KittingStandNumber.one;
                 break;
             case 2:
-                kit.standNum = Kit.StandNum.two;
+                kit.kittingStandNumber = Kit.KittingStandNumber.two;
                 break;
             case 3:
-                kit.standNum = Kit.StandNum.three;
+                kit.kittingStandNumber = Kit.KittingStandNumber.three;
                 break;
             default:
-                kit.standNum = Kit.StandNum.none;
+                kit.kittingStandNumber = Kit.KittingStandNumber.none;
         }
         stateChanged();
     }
@@ -73,68 +72,63 @@ public class PartsAgent extends Agent {
             return true;
         }
 
-        if (!inventory.isEmpty() && kit.status == Kit.Status.empty) {
-            int n = 4;
-            int grip = 0;
-            if (kit.kitNeedsParts < 4) {
-                n = kit.kitNeedsParts;
-            }
-            for (int i = 0; i < kit.parts.length; i++) {
-                if (!kit.parts[i].inKit) {
-                    if (inventory.containsKey(kit.parts[i])) {
-                        pickUpPart(kit.parts[i], grip);
-                        kit.parts[i].inKit = true;
-                        grip++;
-                        if (grip == n) {
-                            putPartsInKit(n);
-                        }
-                        return true;
-                    }
-                }
-            }
+        if (!inventory.isEmpty() && kit.kittingStandNumber!=Kit.KittingStandNumber.none){
+        	pickUpPart(inventory.remove(0));
+        	return true;
         }
 
         if (kit.status == Kit.Status.full) {
             giveKitToKitAgent();
-
+            return true;
         }
+        
+        if (kitNeedsParts.isEmpty()){
+        	giveKitToKitAgent();
+        }
+        	
+        
+        if (!newKit.isEmpty()){
+        	startNewKit(newKit.get(newKit.size()));
+        	newKit.removeAll();
+        	return true;
+        }
+        else
+        	startNewKit(kit);
 
         return false;
     }
 //Actions
 
-    private void setConfiguration() {
-        if (true //                configInfo.hasNewKit()
-                ) {
-            {
-//                kit = configInfo.remove(0);
-                kitagent.msgNeedEmptyKit();
-            }
-            for (int i = 0; i < kit.parts.length; i++) {
-                nest.msgNeedPart(kit.parts[i]);
-
-            }
-        }
-    }
-
     private void giveKitToKitAgent() {
-        kitagent.msgKitIsFull(new Kit());
+        kitagent.msgKitIsComplete(kit);
+    }
+    
+    private void startNewKit(Kit k){
+    	kitNeedsParts.removeAll();
+    	this.kit = k;
+    	for(int i=0; i<k.length; i++){
+    		kitNeedsParts.add(k.parts[i]);
+    	}
+    	kitagent.msgNeedEmptyKit();
+    	for (int i = 0; i < kit.parts.length; i++) {
+            nest.msgNeedPart(kit.parts[i]);
+    	}
+    	stateChanged();
     }
 
-    private void pickUpPart(Part p, int g) {
-        grips[g] = p;
-//        DoPickUpPart(p);
-        kit.kitNeedsParts--;
-        if (kit.kitNeedsParts == 0) {
-            kit.status = Kit.Status.full;
-        }
-        inventory.put(p, inventory.get(p) - 1);
+    private void pickUpPart(Part p) {
+        grips.add(p);
+        DoPickUpPart(p);
+        kitNeedsParts.remove(p);
+        if (grips.size() == 4 || kitNeedsParts.isEmpty())
+        	putPartsInKit();
+        stateChanged();
     }
 
-    private void putPartsInKit(int n) {
-        for (int i = 0; i < n; i++) {
-//            DoPutPartInKit(grips[i]);
-            kit.kitNeedsParts--;
+    private void putPartsInKit() {
+        for (Part p: grips) {
+            DoPutPartInKit(grips.remove(p));
         }
+        
     }
 }
