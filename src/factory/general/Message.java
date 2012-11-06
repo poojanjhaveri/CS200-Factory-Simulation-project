9017510e579ea1d36e10f3e4be742727ec778cb4
YiWei Zheng <yiweizhe@usc.edu>
2012-11-05 17:17:55 -0800

    more messages and manager escription

--- src/factory/general/Message.java
+++ src/factory/general/Message.java
@@ -8,12 +8,13 @@ package factory.general;
  */
 public class Message {
 
-    public static final String CREATE_KIT = "CREATE_KIT"; // manager: sendToServer(Message.CREATE_KIT+":"+asdf)
+    public static final String CREATE_KIT = "CREATE_KIT"; ///< manager: sendToServer(Message.CREATE_KIT+":"+asdf)
     public static final String CLIENT_EXITED = "CLIENT_EXITED";
     public static final String TEST_SERVER = "TEST_SERVER";
     public static final String TEST_CLIENT = "TEST_CLIENT";
     
     //partsmanager section
-    public static final String PULL_PARTS_LIST = "PULL_PARTS_LIST";
-    public static final String DEFINE_NEW_PART = "DEFINE_NEW_PART";
+    public static final String PULL_PARTS_LIST = "PULL_PARTS_LIST";///<simple request from partsmanager to server asking for parts list
+    public static final String PUSH_PARTS_LIST = "PUSH_PARTS_LIST";///<send serialized parts list over from server to partsmanager
+    public static final String DEFINE_NEW_PART = "DEFINE_NEW_PART";///<send a new type of part over to the server
 }
