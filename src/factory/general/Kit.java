05e61ec54863e52c368d166703128b5d8a582d4b
YiWei Zheng <yiweizhe@usc.edu>
2012-11-09 12:32:09 -0800

    astyle

--- src/factory/general/Kit.java
+++ src/factory/general/Kit.java
@@ -58,49 +58,49 @@ public class Kit {
         this.number = (int)(System.currentTimeMillis()/(long)1000);
 
     }
-    public Kit(String n, String d, Integer i){
+    public Kit(String n, String d, Integer i) {
         this.name = n;
         this.description = d;
         this.number = i;
     }
     public String getName()
     {
-	return this.name;
+        return this.name;
     }
     public String getDescription()
     {
-	return this.description;
+        return this.description;
     }
     public Integer getNumber()
     {
-	return this.number;
+        return this.number;
     }
     public void setParts(ArrayList<Part> p)
     {
-	this.parts = p;
+        this.parts = p;
     }
     /**
-@brief string serializes a kit
+    @brief string serializes a kit
      */
     public String serialize()
     {
-	BlueprintParts bpp = new BlueprintParts((ArrayList<Part>)this.parts);
+        BlueprintParts bpp = new BlueprintParts((ArrayList<Part>)this.parts);
         return (this.name.length()+2)+"("+this.name+")"+(2+this.description.length())+"("+this.description+")"+(2+this.number.toString().length())+"("+this.number+")"+(2+bpp.serialize().length())+"("+bpp.serialize()+")";
     }
     /**
-@brief deserializes a string serialized kit
+    @brief deserializes a string serialized kit
      */
     public static Kit deserialize(String serialized)
     {
         ArrayList<String> stringform = Util.deserialize(serialized);
-	if(stringform.size() != 4)
-	    {
-		System.out.println("KIT DESERIALIZATION ERROR: " +serialized + "; ARRAY: "+stringform);
-	    }
-	Kit nk = new Kit(stringform.get(0),stringform.get(1),Integer.parseInt(stringform.get(2)));
-	ArrayList<Part> p = BlueprintParts.deserialize(stringform.get(3));
-	nk.setParts(p);
-	return nk;
+        if(stringform.size() != 4)
+        {
+            System.out.println("KIT DESERIALIZATION ERROR: " +serialized + "; ARRAY: "+stringform);
+        }
+        Kit nk = new Kit(stringform.get(0),stringform.get(1),Integer.parseInt(stringform.get(2)));
+        ArrayList<Part> p = BlueprintParts.deserialize(stringform.get(3));
+        nk.setParts(p);
+        return nk;
     }
     public static void main(String[] args)
     {
@@ -114,9 +114,9 @@ public class Kit {
         p = new Part("alfalfa","heyo");
         p.setFilename("gogo.png");
         bp.addPart(p);
-	System.out.println(bp.serialize());
+        System.out.println(bp.serialize());
 
-	Kit des = Kit.deserialize(bp.serialize());
-	System.out.println(des.getName() + " " + des.getDescription() + " " + des.getNumber());
+        Kit des = Kit.deserialize(bp.serialize());
+        System.out.println(des.getName() + " " + des.getDescription() + " " + des.getNumber());
     }
 }
