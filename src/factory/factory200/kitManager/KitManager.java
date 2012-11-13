03b35b4cb3e98b78b4c1c1e18e38183dc5f0b78c
Poojan Jhaveri <poojanjhaveri@hotmail.com>
2012-11-12 22:27:29 -0800

    Layout for Kit manager - create kit

--- src/factory/factory200/kitManager/KitManager.java
+++ src/factory/factory200/kitManager/KitManager.java
@@ -1,11 +1,15 @@
 package factory.factory200.kitManager;
 
 import factory.general.BlueprintKits;
+import factory.general.BlueprintParts;
 import factory.general.Manager;
 import factory.general.Message;
 import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -27,7 +31,8 @@ public class KitManager extends Manager {
 	/**
 	 * @param args
 	 */
-          BlueprintKits bp;///<contains list of kits for serialization
+          BlueprintKits bpkit;
+          BlueprintParts bppart;///<contains list of kits for serialization
           JPanel mainpanel;
           JTabbedPane tabbedPane;
           
@@ -38,19 +43,24 @@ public class KitManager extends Manager {
           JPanel uk_main;
           JPanel dk_main;
           JTextField kitname;
+          JComboBox create_combo;
+          
+          private ArrayList partlist = new ArrayList();
           
     
     
 	
          public KitManager()
          {
-            this.bp = new BlueprintKits();
+            this.bpkit = new BlueprintKits();
+            this.bppart = new BlueprintParts();
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-             setBounds(100, 100, 450, 300);
-
+             setBounds(100, 100, 600, 400);
+            this.update();  
+           
             prepareMainPane();
 	//	this.mcon.out(Message.PULL_PARTS_LIST);
-              this.update();
+              
          }
 	
 	
@@ -86,6 +96,10 @@ public class KitManager extends Manager {
             ck_main.setLayout(new GridBagLayout());
             GridBagConstraints c = new GridBagConstraints();
             
+            c.ipady=10;
+            c.ipadx=10;
+            c.insets=new Insets(0, 0, 15, 0);;
+          
             
             c.gridx=0;
             c.gridy=0;
@@ -96,10 +110,57 @@ public class KitManager extends Manager {
             kitname = new JTextField(15);
             ck_main.add(kitname,c);
             
+            c.gridx=0;
+            c.gridy=1;
+            ck_main.add(new JLabel("Select part:"),c);
+            
+ 
+            c.gridx=1;
+            c.gridy=1;
+            
+            
+            create_combo = new JComboBox(); // parts list
+            for(int j=0;j<this.bppart.getSize();j++){
+    		 create_combo.addItem(this.bppart.getPartAt(j).getName()); 
+               
+            }
+            ck_main.add(create_combo,c);
+            
+            c.gridx=0;
+            c.gridy=2;
+            c.gridwidth=2;
+            
+            JPanel partgrid = new JPanel();
+            partgrid.setLayout(new GridLayout(2,4));
+            
+            JButton b1=new JButton();
+            JButton b2=new JButton();
+            JButton b3=new JButton();
+            JButton b4=new JButton();
+            JButton b5=new JButton();
+            JButton b6=new JButton();
+            JButton b7=new JButton();
+            JButton b8=new JButton();
+            
+            partgrid.add(b1);
+            partgrid.add(b2);
+            partgrid.add(b3);
+            partgrid.add(b4);
+            partgrid.add(b5);
+            partgrid.add(b6);
+            partgrid.add(b7);
+            partgrid.add(b8);
+            ck_main.add(partgrid,c);
+            
+            c.gridx=0;
+            c.gridy=3;
+            c.gridwidth=2;
+            JButton submitbutton = new JButton("Create Kit");
+            ck_main.add(submitbutton,c);
+            
+            
+    
             createkit.add(ck_main);
-            
-            
-            
             
             
             
@@ -126,9 +187,16 @@ public class KitManager extends Manager {
      * @brief sends an update request which should result in the parts list being updated
      */
         public void update() {
+            
+            this.mcon.out(Message.PULL_PARTS_LIST);
+            System.out.println("Updates parts list from the server");
+            
+            
             this.mcon.out(Message.PULL_KITS_LIST);
             System.out.println("Updates kits list from the server");
          }
+        
+        
         
 
 }
