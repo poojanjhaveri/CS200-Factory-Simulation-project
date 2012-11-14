adeef6b667e037a25118d5482b9273cd6fdb7a38
David Zhang <dzhang21@gmail.com>
2012-11-14 13:01:02 -0800

    small part changes

--- src/factory/general/GUIPart.java
+++ src/factory/general/GUIPart.java
@@ -6,9 +6,8 @@ public class GUIPart extends Drawable{
 
     private ImageIcon img;
 
-    public GUIPart(Integer x, Integer y, Double r, ImageIcon i)
-    {
-	super(x,y,r,i);
+    public GUIPart(Integer x, Integer y, Double r, ImageIcon i) {
+    	super(x,y,r,i);
     }
 
 
