384931cee25c536910277520aae08ac8b927090f
YiWei Zheng <yiweizhe@usc.edu>
2012-11-15 16:47:33 -0800

    astyle

--- src/factory/general/Kit.java
+++ src/factory/general/Kit.java
@@ -14,12 +14,14 @@ public class Kit {
 
     /** CS 201 stuff */
     public enum Status {
+
         empty, full, verified, error
     };
+
     public static enum StandNum {
+
         none, zero, one, two
     };
-
     public String name;
     public Status status;
     public StandNum standNum;
@@ -36,6 +38,7 @@ public class Kit {
     public void addPart(Part p) {
         parts.add(p);
     }
+
     public int getSize() {
         return parts.size();
     }
@@ -43,116 +46,110 @@ public class Kit {
     public Part getPart(int i) {
         return parts.get(i);
     }
-
     /** End CS 201 stuff*/
-
-
     /*CS200*/
     String description;
     Integer number;
 
-    public Kit(String n, String d)
-    {
-        
+    public Kit(String n, String d) {
+
         // Kit name 
         this.name = n;
-        this.description=d;
-        this.number = (int)(System.currentTimeMillis()/(long)1000);
+        this.description = d;
+        this.number = (int) (System.currentTimeMillis() / (long) 1000);
     }
+
     public Kit(String n, String d, Integer i) {
         this.name = n;
         this.number = i;
         this.description = d;
     }
-    public String getName()
-    {
+
+    public String getName() {
         return this.name;
     }
-    
-    
-    public String getDescription()
-    {
+
+    public String getDescription() {
         return this.description;
-    } 
-    public Integer getNumber()
-    {
+    }
+
+    public Integer getNumber() {
         return this.number;
     }
-    public void setParts(ArrayList<Part> p)
-    {
+
+    public void setParts(ArrayList<Part> p) {
         this.parts = p;
     }
+
     /**
     @brief string serializes a kit
      */
-    public String serialize()
-    {
-        if(this.parts == null)
-        {
+    public String serialize() {
+        if (this.parts == null) {
             System.out.println("CRITICAL ERROR SERIALIZING KIT: PARTS ARRAY WAS NEVER SET!");
-        return null;
+            return null;
         }
-        BlueprintParts bpp = new BlueprintParts((ArrayList<Part>)this.parts);
-        return (this.name.length()+2)+"("+this.name+")"+(2+this.description.length())+"("+this.description+")"+(2+this.number.toString().length())+"("+this.number+")"+(2+bpp.serialize().length())+"("+bpp.serialize()+")";
+        BlueprintParts bpp = new BlueprintParts((ArrayList<Part>) this.parts);
+        return (this.name.length() + 2) + "(" + this.name + ")" + (2 + this.description.length()) + "(" + this.description + ")" + (2 + this.number.toString().length()) + "(" + this.number + ")" + (2 + bpp.serialize().length()) + "(" + bpp.serialize() + ")";
     }
+
     /**
     @brief deserializes a string serialized kit
      */
-    public static Kit deserialize(String serialized)
-    {
+    public static Kit deserialize(String serialized) {
         ArrayList<String> stringform = Util.deserialize(serialized);
-        if(stringform.size() != 4)
-        {
-            System.out.println("KIT DESERIALIZATION ERROR: " +serialized + "; ARRAY: "+stringform);
+        if (stringform.size() != 4) {
+            System.out.println("KIT DESERIALIZATION ERROR: " + serialized + "; ARRAY: " + stringform);
         }
-        Kit nk = new Kit(stringform.get(0),stringform.get(1),Integer.parseInt(stringform.get(2)));
+        Kit nk = new Kit(stringform.get(0), stringform.get(1), Integer.parseInt(stringform.get(2)));
         ArrayList<Part> p = BlueprintParts.deserialize(stringform.get(3));
         nk.setParts(p);
         return nk;
     }
-    public void debug()
-    {
+
+    public void debug() {
         System.out.println(this.getName() + " " + this.getDescription() + " " + this.getNumber());
-	for(int i = 0; i != this.parts.size(); i++)
-	    {
-		System.out.println(this.parts.get(0).serialize());
-	    }
+        for (int i = 0; i != this.parts.size(); i++) {
+            System.out.println(this.parts.get(0).serialize());
+        }
     }
-    public static void main(String[] args)
-    {
-        Part p = new Part("part1","is a part");
+
+    public static void main(String[] args) {
+        Part p = new Part("part1", "is a part");
         p.setFilename("part1.png");
-        Kit bp = new Kit("kit1","I am a kit lollol");
+        Kit bp = new Kit("kit1", "I am a kit lollol");
         bp.addPart(p);
-        p = new Part("part2","is (not) a part");
+        p = new Part("part2", "is (not) a part");
         p.setFilename("part2.png");
         bp.addPart(p);
-        p = new Part("alfalfa","heyo");
+        p = new Part("alfalfa", "heyo");
         p.setFilename("gogo.png");
         bp.addPart(p);
-	bp.debug();
+        bp.debug();
         System.out.println("SERIALIZED KIT");
         System.out.println(bp.serialize());
 
         Kit des = Kit.deserialize(bp.serialize());
         System.out.println("DESERIALIZED KIT");
-	System.out.println(des.serialize());
-	des.debug();
-	if(des.serialize().equals(bp.serialize()))
-	    {
-		System.out.println("Test passed");
-	    }else System.out.println("TEST FAILED");
-        
+        System.out.println(des.serialize());
+        des.debug();
+        if (des.serialize().equals(bp.serialize())) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("TEST FAILED");
+        }
+
         System.out.println("Testing empty parts kit");
-        des = new Kit("mahkit","its my kit");
+        des = new Kit("mahkit", "its my kit");
         des.setParts(new ArrayList<Part>());
         String s = des.serialize();
-        System.out.println("Serialized:"+s);
+        System.out.println("Serialized:" + s);
         Kit newkit = Kit.deserialize(s);
-        System.out.println("Serialized deserialized serialized kit:"+newkit.serialize());
-        if(s.equals(newkit.serialize()))
-        {
+        System.out.println("Serialized deserialized serialized kit:" + newkit.serialize());
+        if (s.equals(newkit.serialize())) {
             System.out.println("Test passed");
-        }else System.out.println("TEST FAILED");
+        } else {
+            System.out.println("TEST FAILED");
+        }
     }
 }
