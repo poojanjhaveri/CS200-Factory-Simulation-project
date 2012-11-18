ad5909f6a2e867add67cd5962fb1f06df926c915
David Zhang <dzhang21@gmail.com>
2012-11-18 15:00:27 -0800

    exception msg update

--- src/factory/general/ManagerConnection.java
+++ src/factory/general/ManagerConnection.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 /**
@@ -92,15 +93,19 @@ public class ManagerConnection implements Runnable {
         try {
             String msg = in.readLine();
             this.mgr.processMessage(msg);
-        }  catch (NullPointerException e) {
+        } catch (NullPointerException e) {
         	e.printStackTrace();
-        	
-//        	System.out.println("A client has exited prematurely while other clients were still connected.");
-//        	System.out.println("Shutting down.");
+        	System.out.println("Null pointer. A client may have exited prematurely while other clients were still connected.");
+        	System.out.println("Shutting down.");
         	System.exit(0);
-        	
+        } catch (SocketException e) {
+        	System.out.println("Socket exception. A client may have exited prematurely while other clients were still connected.");
+        	System.out.println("Shutting down.");
+        	System.exit(0);
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("wtf?");
+            System.exit(0);
         }
     }
 }
