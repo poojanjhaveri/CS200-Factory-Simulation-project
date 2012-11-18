6e297c93a423de7e839a4691ed5d43f3ed4750bf
Dongyoung Jung <dongyouj@usc.edu>
2012-11-18 13:56:30 -0800

    deleted many files

--- src/factory/factory200/laneManager/ClientSide/LMDrawableBin.java
+++ src/factory/factory200/laneManager/ClientSide/LMDrawableBin.java
@@ -11,14 +11,15 @@ public class LMDrawableBin {
 
 	private int binX, binY;
 	private ImageIcon binImage;
-	private static ImageIcon bin1 = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox1.jpg") );
-	private static ImageIcon bin2 = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox2.jpg") );
-	private static ImageIcon bin3 = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox3.jpg") );
-	private static ImageIcon bin4 = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox4.jpg") );
-	private static ImageIcon bin5 = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox5.jpg") );
-	private static ImageIcon bin6 = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox6.jpg") );
-	private static ImageIcon bin7 = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox7.jpg") );
-	private static ImageIcon bin8 = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox8.jpg") );
+	private static ImageIcon bin1Image = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox1.png") );
+	private static ImageIcon bin2Image = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox2.png") );
+	private static ImageIcon bin3Image = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox3.png") );
+	private static ImageIcon bin4Image = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox4.png") );
+	private static ImageIcon bin5Image = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox5.png") );
+	private static ImageIcon bin6Image = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox6.png") );
+	private static ImageIcon bin7Image = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox7.png") );
+	private static ImageIcon bin8Image = new ImageIcon( LMDrawableBin.class.getResource("./pics/binBox8.png") );
+	private static ImageIcon emptyBinImage = new ImageIcon( LMDrawableBin.class.getResource("./pics/emptybox.png") );
 	
 	private Boolean binExist = false;
 		
@@ -28,15 +29,19 @@ public class LMDrawableBin {
 	}
 	
 	public void setBinImage(int binNum){
-		if( binNum == 0 ){ binImage = bin1; }
-		else if( binNum == 1 ){ binImage = bin2; }
-		else if( binNum == 2 ){ binImage = bin3; }
-		else if( binNum == 3 ){ binImage = bin4; }
-		else if( binNum == 4 ){ binImage = bin5; }
-		else if( binNum == 5 ){ binImage = bin6; }
-		else if( binNum == 6 ){ binImage = bin7; }
-		else if( binNum == 7 ){ binImage = bin8; }
+		if( binNum == 0 ){ binImage = bin1Image; }
+		else if( binNum == 1 ){ binImage = bin2Image; }
+		else if( binNum == 2 ){ binImage = bin3Image; }
+		else if( binNum == 3 ){ binImage = bin4Image; }
+		else if( binNum == 4 ){ binImage = bin5Image; }
+		else if( binNum == 5 ){ binImage = bin6Image; }
+		else if( binNum == 6 ){ binImage = bin7Image; }
+		else if( binNum == 7 ){ binImage = bin8Image; }
 		binExist = true;
+	}
+	
+	public void emptyBin(){
+		binImage = emptyBinImage;
 	}
 	
 	public void purgeBin(){
