08a32d617a8c05e07197c68b39941d1373ab4684
David Zhang <dzhang21@gmail.com>
2012-11-18 23:35:56 -0800

    comments clarify

--- src/factory/general/HandleAManager.java
+++ src/factory/general/HandleAManager.java
@@ -177,20 +177,18 @@ public class HandleAManager implements Runnable {
         	System.out.println("Undefining part " + id);
         	this.server.getFactoryState().removeKitById(id);
         }else if(msg.contains(Message.PUSH_PRODUCTION_QUEUE)) {
-        	
-        	// TODO: THIS IS TEMPORARY JUST FOR TESTING THE WHOLE THING INTEGRATED; CHANGE TO REMOVE startInteractionSequence
         	ArrayList<Kit> queue = new ArrayList<Kit>();
         	ArrayList<String> deserialized = Util.deserialize(this.grabParameter(msg));
         	for(int i = 0; i != deserialized.size(); i++) {
         		queue.add(Kit.deepClone(this.server.getFactoryState().getKitById(Integer.parseInt(deserialized.get(i)))));
         		System.out.println(deserialized.get(i));
         	}
-
         	this.server.getConveyorAgent().generateKit(queue.size()); // * This generates 10 new kits, among other things if you pass string... *
         	this.server.getPartsAgent().msgHereIsKit(queue);
-        	//this.server.startInteractionSequence();
+        	
+        	//this.server.startInteractionSequence(); // just for testing
 
-        	System.out.println("BEGINNING PRODUCTION CYCLE WOOOOOOT (size "+queue.size() + ")");
+        	p.println("BEGINNING PRODUCTION CYCLE WOOOOOOT (size "+queue.size() + ")");
                         	queue.get(0).debug();
 
         }
