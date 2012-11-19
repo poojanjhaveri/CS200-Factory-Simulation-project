54d972eff7815003732bd33d5a94c9d4017d1a42
Kevin Macwan <kmacwan@usc.edu>
2012-11-18 23:18:25 -0800

    commit

--- src/factory/general/Server.java
+++ src/factory/general/Server.java
@@ -267,7 +267,7 @@ public class Server {
             kits.add(kit);
             kits.add(kit);
             kits.add(kit);
-            
+            kits.get(0).debug();
             // TODO: *Put this wherever the FPM sends the signal to create (generate) kits
         //    conveyorAgent.generateKit(10); // * This generates 10 new kits, among other things if you pass string... *
 
