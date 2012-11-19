829ebd0a9387df8653ad10b79e9fb9de16baeb94
Kevin Macwan <kmacwan@usc.edu>
2012-11-18 18:33:19 -0800

    committ

--- src/factory/general/Server.java
+++ src/factory/general/Server.java
@@ -116,7 +116,7 @@ public class Server {
 			System.exit(0);
 		}
 		
-		for (int i=0 ; i<1 ; i++){ // For Testing By Dongyoung, if want to need communicate n managers, change into for(int i=0 ; i<n ; i++)
+		for (int i=0 ; i<2 ; i++){ // For Testing By Dongyoung, if want to need communicate n managers, change into for(int i=0 ; i<n ; i++)
 		//while(true){
 			// Continuously check for a new client for which to create a thread
 			try {
@@ -131,7 +131,7 @@ public class Server {
 			System.out.println("A client has connected");
 		}
 		
-		try{  Thread.sleep(1000);  } catch(Exception e){}   // For Testing By Dongyoung
+	//	try{  Thread.sleep(1000);  } catch(Exception e){}   // For Testing By Dongyoung
 		prepareAllAgents(); // Prepare all agents; based on AgentMain.java      // For Testing By Dongyoung
 	}
 
