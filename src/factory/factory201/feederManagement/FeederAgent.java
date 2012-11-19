92a5ac24473d355d38e34c02d7ab977758ed19c0
Kevin Macwan <kmacwan@usc.edu>
2012-11-18 20:24:00 -0800

    mergeeeee

--- src/factory/factory201/feederManagement/FeederAgent.java
+++ src/factory/factory201/feederManagement/FeederAgent.java
@@ -309,7 +309,6 @@ public class FeederAgent extends Agent implements Feeder {
          */
         //	 this.client.sendMessage("Message");
 
-        if (client != null) {
             if (LMServer.getForAgentFeeder() == null) {
                 return;
             }
@@ -324,9 +323,7 @@ public class FeederAgent extends Agent implements Feeder {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-        } else {
-            print("Client = null");
-        }
+         
     }
 
     private void dosendPartToRightLane(myParts p) {
@@ -337,7 +334,6 @@ public class FeederAgent extends Agent implements Feeder {
          e.printStackTrace();
          }
          */
-        if (client != null) {
             LMServer.getForAgentFeeder().setDiverterSwitchRight(feederNum);
 
             LMServer.getForAgentGantryRobot().putBin(p.part.type, p.quantity, feederNum);
@@ -349,9 +345,7 @@ public class FeederAgent extends Agent implements Feeder {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
-        } else {
-            print("Client = null");
-        }
+         
     }
 
 
