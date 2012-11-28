83728a4bb353e575c6846fec9c158b28e87c47b8
Yuting Liu <liu358@usc.edu>
2012-11-27 19:10:43 -0800

    fpm_grm_fix

--- src/factory/factory200/factoryProductionManager/GantryRobotManager/GUIGantryRobot.java
+++ src/factory/factory200/factoryProductionManager/GantryRobotManager/GUIGantryRobot.java
@@ -47,11 +47,11 @@ public class GUIGantryRobot extends GUIRobot{
     Integer moveto;///<where the gantry robot is heading towards
 
     public GUIGantryRobot() {
-	super(GantryRobotManager.ROBOT_INITIAL_X,GantryRobotManager.ROBOT_INITIAL_Y,GUIGantryRobot.IMAGE_PATH);
-	this.extended = false;
-	this.hasbin = false;
-	this.bin = null;
-	this.setConstants(GantryRobotManager.ROBOT_VELOCITY_X, GantryRobotManager.ROBOT_VELOCITY_Y, GantryRobotManager.ROBOT_TURN_RATE);
+    	super(GantryRobotManager.ROBOT_INITIAL_X,GantryRobotManager.ROBOT_INITIAL_Y,GUIGantryRobot.IMAGE_PATH);
+    	this.extended = false;
+    	this.hasbin = false;
+    	this.bin = null;
+    	this.setConstants(GantryRobotManager.ROBOT_VELOCITY_X, GantryRobotManager.ROBOT_VELOCITY_Y, GantryRobotManager.ROBOT_TURN_RATE);
     }
     /**
     @brief sets a new feeder destination for the gantry robot
