80672566346aa4f2b07cd0c274a788e2b15d7ce1
Alex Young <alexyoung1992@gmail.com>
2012-11-27 20:57:47 -0800

    n

---
+++ run_bash_nb.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "STARTING SERVER"
+java factory.general.Server &
+sleep 4
+echo "BEGINNING ALL MANAGERS"
+java -cp build/classes factory.factory200.partsManager.PartsManager &
+sleep 1
+java -cp build/classes factory.factory200.kitManager.KitManager &
+sleep 1
+java -cp build/classes factory.factory200.gantryRobotManager.GantryRobotManager &
+sleep 1
+java -cp build/classes factory.factory200.laneManager.ClientSide.V1LaneManagerCompileClient &
+sleep 1
+java -cp build/classes factory.factory200.kitAssemblyManager.KitAssemblyManager &
+sleep 1
+java -cp build/classes factory.factory200.factoryProductionManager.FactoryProductionManager &
