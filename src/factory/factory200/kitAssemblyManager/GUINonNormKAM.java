8caa247a17417f85d5baaafca060a17e45e2b3d1
Poojan Jhaveri <poojanjhaveri@hotmail.com>
2012-11-29 15:03:01 -0800

    KAM tabbed pane now will start adding

---
+++ src/factory/factory200/kitAssemblyManager/GUINonNormKAM.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factory.factory200.kitAssemblyManager;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Poojan Jhaveri
+ * @brief : GUI class for non-normative scenario associated with KIT assembly manager
+ * 
+ * 
+ * 
+ */
+public class GUINonNormKAM  extends JPanel {
+    
+    
+    
+    
+}
