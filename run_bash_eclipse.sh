830c79fc3eb34aa0455f642d97e8e3a0d1f6ada1
David Zhang <dzhang21@gmail.com>
2012-11-30 14:34:02 -0800

    bash_eclipse change

--- run_bash_eclipse.sh
+++ run_bash_eclipse.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-###################
-#Factory run file #
-#Roy Zheng 2012   #
-###################
+#################################
+# Factory run file 				#
+# Roy Zheng, David Zhang 2012   #
+#################################
 
 echo "STARTING SERVER"
 java -cp bin factory.general.Server &
 sleep 4
 echo "BEGINNING ALL MANAGERS"
 java -cp bin factory.factory200.partsManager.PartsManager &
-sleep 1
+sleep 2
 java -cp bin factory.factory200.kitManager.KitManager &
-sleep 1
+sleep 2
 java -cp bin factory.factory200.gantryRobotManager.GantryRobotManager &
-sleep 1
+sleep 2
 java -cp bin factory.factory200.laneManager.ClientSide.V1LaneManagerCompileClient &
-sleep 1
+sleep 2
 java -cp bin factory.factory200.kitAssemblyManager.KitAssemblyManager &
-sleep 1
+sleep 2
 java -cp bin factory.factory200.factoryProductionManager.FactoryProductionManager &
