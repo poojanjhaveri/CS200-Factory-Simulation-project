1cadcea3cf4da22f299ddafe9514a5c0f19ec3af
David Zhang <dzhang21@gmail.com>
2012-11-30 20:33:47 -0800

    .sh changes - iffy, still

--- run_bash_eclipse.sh
+++ run_bash_eclipse.sh
@@ -9,13 +9,13 @@ java -cp bin factory.general.Server &
 sleep 4
 echo "BEGINNING ALL MANAGERS"
 java -cp bin factory.factory200.partsManager.PartsManager &
-sleep 2
+sleep 1
 java -cp bin factory.factory200.kitManager.KitManager &
-sleep 2
+sleep 1
 java -cp bin factory.factory200.gantryRobotManager.GantryRobotManager &
-sleep 2
-java -cp bin factory.factory200.laneManager.ClientSide.V1LaneManagerCompileClient &
-sleep 2
+sleep 1
+java -cp bin factory.factory200.laneManager.ClientSide.V2LaneManager &
+sleep 1
 java -cp bin factory.factory200.kitAssemblyManager.KitAssemblyManager &
-sleep 2
+sleep 1
 java -cp bin factory.factory200.factoryProductionManager.FactoryProductionManager &
