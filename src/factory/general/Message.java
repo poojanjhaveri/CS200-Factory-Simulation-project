97f506a2ac704c7a5f9f9fb14c3b6ad1551b3602
YiWei Zheng <yiweizhe@usc.edu>
2012-12-01 15:30:06 -0800

    nestup/down

--- src/factory/general/Message.java
+++ src/factory/general/Message.java
@@ -65,6 +65,8 @@ public class Message {
     public static final String KAM_ACTION_STABILIZE_NEST = "KAM_ACTION_STABILIZE_NEST";
     public static final String KAM_ACTION_UNPILE_NEST = "KAM_ACTION_UNPILE_NEST";
     public static final String EARLY_CAMERA_FLASH = "EARLY_CAMERA_FLASH";
+    public static final String NEST_UP = "NEST_UP";
+    public static final String NEST_DOWN = "NEST_DOWN";
     //    public static final String KAM_FINISH_DROP_OFF_FULL_KIT = "KAM_FINISH_DROP_OFF_FULL_KIT";
     //    public static final String KAM_FINISH_MOVE_TO_NEST = "KAM_FINISH_MOVE_TO_NEST";
     //public static final String KAM_FINISH_DROP_OFF_PARTS = "KAM_FINISH_DROP_OFF_PARTS";
