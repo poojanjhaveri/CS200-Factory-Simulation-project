97dc3e26b28b0863c86e8ccc4f2ed423820a308f
Dongyoung Jung <dongyouj@usc.edu>
2012-12-01 22:05:38 -0800

    signals from KAM

--- src/factory/factory200/laneManager/ServerSide/LMCameraData.java
+++ src/factory/factory200/laneManager/ServerSide/LMCameraData.java
@@ -8,7 +8,7 @@ public class LMCameraData {
 	
 	private LMServerMain serverMain;
 	private String signal = "";
-	private boolean PiledPart = false;
+	//private boolean PiledPart = false;
 	private int nestCameraNum;
 
 	public LMCameraData( int nestCameraNum, LMServerMain serverMain ){
@@ -22,25 +22,10 @@ public class LMCameraData {
 		serverMain.sendToLM(signal);
 		serverMain.sendToFPM(signal);
 		
-		// Non-normative Scenario 1 : Signal to Agent(Bad Parts)
+		// Non-normative Scenario : Signal to Agent(Bad Parts)
 		if( serverMain.getPartData().checkAllBadParts(nestCameraNum) == true ){
 			System.out.println("BAD PARTS ON NEST " + nestCameraNum );
 			serverMain.getCameraAgent().msgAllPartsBad(nestCameraNum);
 		}
-		
-		// Non-normative Scenario 3 : Signal to Agent(Piled Parts)
-		if( PiledPart == true ){
-			System.out.println("PARTS PILED UP " + nestCameraNum );
-			serverMain.getCameraAgent().msgPartsPiledUp(nestCameraNum);
-			PiledPart = false;
-		}
-	}
-	
-	public void startPiledPart(){
-		PiledPart = true;
-	}
-	
-	public void stopPiledPart(){
-		PiledPart = false;
 	}
 }
