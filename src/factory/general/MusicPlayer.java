4c6c576787e3c293f74088dd4dda52297984dffb
David Zhang <dzhang21@gmail.com>
2012-12-02 18:22:34 -0800

    import fix in music player

--- src/factory/general/MusicPlayer.java
+++ src/factory/general/MusicPlayer.java
@@ -1,19 +1,15 @@
 package factory.general;
 
 import javax.sound.sampled.*;
-import java.io.*;
 
    import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 /**
